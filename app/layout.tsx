7165add4b927c1c5694adad6476a9a254762fe84
disin8 <dmitry.sin8@gmail.com>
2024-06-07 12:52:08 +0300

    fix: :bug: prefers color scheme fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#0d0d0d' },
   ],
 }
 
