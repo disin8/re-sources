4bdae9f5543a5a85a81404e88af0988227043ab5
disin8 <dmitry.sin8@gmail.com>
2024-06-07 11:19:47 +0300

    feat: :sparkles: mobile nav; responsive brand

---
+++ components/sidebar-footer.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { Icons } from './icons'
+import { sidebar } from '@/config/sidebar'
+import { siteConfig } from '@/config/site'
+
+export function SidebarFooter() {
+  return (
+    <div className="w-full pt-2 flex items-center flex-col gap-2 bg-background text-gray-600 dark:text-neutral-400 text-sm">
+      <div className="w-full flex gap-4">
+        <Button className="w-full justify-between" asChild variant="outline">
+          <Link href={siteConfig.links.github}>
+            GitHub
+            <Icons.external width="14px" height="14px" />
+          </Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href={siteConfig.links.twitter}>
+            <Icons.twitter width="14px" height="14px" />
+          </Link>
+        </Button>
+      </div>
+      <Button className="w-full justify-between" asChild variant="outline">
+        <Link href="mailto:hi@re-sources.com">
+          hi@re-sources.com
+          <Icons.mail width="14px" height="14px" />
+        </Link>
+      </Button>
+      <span className="text-sm text-gray-400 leading-5">© 2024 re • sources</span>
+    </div>
+  )
+}
